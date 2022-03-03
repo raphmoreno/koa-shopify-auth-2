bb494b81a9ceef6e8b1c7c41544b8deb84bbb34e
Raphael Gomez <raphael.gomezserfaty@gmail.com>
2022-03-03 12:22:07 +0100

    initial commit

---
+++ src/verify-request/utilities.ts
@@ -0,0 +1,34 @@
+import {Context} from 'koa';
+
+import Shopify from '@shopify/shopify-api';
+
+import {Routes} from './types';
+import {AccessMode} from '../types';
+import {DEFAULT_ACCESS_MODE} from '../auth';
+
+export function redirectToAuth(
+  {fallbackRoute, authRoute}: Routes,
+  ctx: Context,
+) {
+  const {
+    query: {shop},
+  } = ctx;
+
+  const routeForRedirect =
+    shop == null ? fallbackRoute : `${authRoute}?shop=${shop}`;
+
+  ctx.redirect(routeForRedirect);
+}
+
+export async function clearSession(ctx: Context, accessMode: AccessMode = DEFAULT_ACCESS_MODE) {
+  try {
+    await Shopify.Utils.deleteCurrentSession(ctx.req, ctx.res, accessMode === 'online');
+  }
+  catch (error) {
+    if (error instanceof Shopify.Errors.SessionNotFound) {
+      // We can just move on if no sessions were cleared
+    } else {
+      throw error;
+    }
+  }
+}
---
+++ src/verify-request/verify-token.ts
@@ -0,0 +1,73 @@
+import Shopify from '@shopify/shopify-api';
+import { Session } from '@shopify/shopify-api/dist/auth/session';
+
+import {Context} from 'koa';
+
+import {AccessMode, NextFunction} from '../types';
+import {TEST_COOKIE_NAME, TOP_LEVEL_OAUTH_COOKIE_NAME} from '../index';
+
+import {Routes} from './types';
+import {redirectToAuth} from './utilities';
+import {DEFAULT_ACCESS_MODE} from '../auth';
+import { HttpResponseError } from '@shopify/shopify-api/dist/error';
+
+export const REAUTH_HEADER = 'X-Shopify-API-Request-Failure-Reauthorize';
+export const REAUTH_URL_HEADER = 'X-Shopify-API-Request-Failure-Reauthorize-Url';
+
+export function verifyToken(routes: Routes, accessMode: AccessMode = DEFAULT_ACCESS_MODE, returnHeader = false) {
+  return async function verifyTokenMiddleware(
+    ctx: Context,
+    next: NextFunction,
+  ) {
+    let session: Session | undefined;
+    session = await Shopify.Utils.loadCurrentSession(ctx.req, ctx.res, accessMode === 'online');
+
+    if (session) {
+      const scopesChanged = !Shopify.Context.SCOPES.equals(session.scope);
+
+      if (!scopesChanged && session.accessToken && (!session.expires || session.expires >= new Date())) {
+        try {
+          // make a request to make sure oauth has succeeded, retry otherwise
+          const client = new Shopify.Clients.Rest(session.shop, session.accessToken)
+          await client.get({ path: "metafields", query: {'limit': 1} }) 
+
+          ctx.cookies.set(TOP_LEVEL_OAUTH_COOKIE_NAME);
+          await next();
+          return;
+        } catch(e) {
+          if (e instanceof HttpResponseError && e.code == 401){
+              // only catch 401 errors
+          } else {
+            throw e
+          }
+        }
+      }
+    }
+
+    ctx.cookies.set(TEST_COOKIE_NAME, '1');
+
+    if (returnHeader) {
+      ctx.response.status = 403;
+      ctx.response.set(REAUTH_HEADER, '1');
+
+      let shop: string|undefined = undefined;
+      if (session) {
+        shop = session.shop;
+      } else if (Shopify.Context.IS_EMBEDDED_APP) {
+        const authHeader: string|undefined = ctx.req.headers.authorization;
+        const matches = authHeader?.match(/Bearer (.*)/);
+        if (matches) {
+          const payload = Shopify.Utils.decodeSessionToken(matches[1]);
+          shop = payload.dest.replace('https://', '');
+        }
+      }
+
+      if (shop) {
+        ctx.response.set(REAUTH_URL_HEADER, `${routes.authRoute}?shop=${shop}`);
+      }
+      return;
+    } else {
+      redirectToAuth(routes, ctx);
+    }
+  };
+}
